12e6ef266c1aaf930f383337dbc890202121bc43
Khadija <kdarkaoui56@gmail.com>
2025-01-15 10:09:10 +0100

    V4

---
+++ ProjetMVC/Models/Myctx.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjetMVC.Models
+{
+    public class Myctx : DbContext
+    {
+        public Myctx(DbContextOptions<Myctx> options) : base(options)
+        {
+        }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Produit> Produit { get; set; }
+        public DbSet<Catalogue> Catalogue { get; set; }
+
+    }
+}

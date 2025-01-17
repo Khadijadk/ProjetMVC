fea75e1caa1ed7b4e5f1cb722360cdd79a68eb2a
insta <aliouned007@gmail.com>
2025-01-17 11:48:38 +0100

    controller

--- ProjetMVC/Controllers/ProduitController.cs
+++ ProjetMVC/Controllers/ProduitController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjetMVC.Models;
 
 namespace ProjetMVC.Controllers
 {
     public class ProduitController : Controller
     {
-        public IActionResult Ajouter()
+        private readonly Myctx _context;
+        public ProduitController(Myctx context)
+        {
+            _context = context;
+        }
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Id,Nom,Prix")] Produit product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
         }
         public IActionResult Modifier()
         {

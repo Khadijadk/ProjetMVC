e00d9ca7c1e3734b83694e1484b3cf376f5d4dca
Khadija <kdarkaoui56@gmail.com>
2025-01-17 11:04:48 +0100

    Rech produit

--- ProjetMVC/Models/Produit.cs
+++ ProjetMVC/Models/Produit.cs
@@ -1,10 +1,1 @@
-﻿namespace ProjetMVC.Models
-{
-    public class Produit
-    {
-        public int Id { get; set; }
-        public string Nom { get; set; }
-        public decimal Prix { get; set; }
-
-    }
-}
+﻿

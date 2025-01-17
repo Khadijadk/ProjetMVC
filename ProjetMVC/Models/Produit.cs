7c95fad8c01bfdc2b6e0ba8f378f3c3b0c9fb959
insta <aliouned007@gmail.com>
2025-01-17 11:00:50 +0100

    formulaires client

--- ProjetMVC/Models/Produit.cs
+++ ProjetMVC/Models/Produit.cs
@@ -1,9 +1,17 @@
-﻿namespace ProjetMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetMVC.Models
 {
     public class Produit
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public string Nom { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
         public decimal Prix { get; set; }
 
     }
